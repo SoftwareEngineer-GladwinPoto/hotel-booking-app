d6b98b5dff3ab9561752366105270020bf7579b3
Gladwin Poto <potogladwin15@gmail.com>
2024-08-31 00:38:10 +0200

    Added some changes to the previous code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   index: {
-    proxy: {
-      '/api' : 'http://localhost:5000'
-    }
-  },
+
   plugins: [react()],
 })
