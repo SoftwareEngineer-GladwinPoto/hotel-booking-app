524fd2c4abc56df1e44f02055ce0011e229e480a
Gladwin Poto <potogladwin15@gmail.com>
2024-09-02 11:40:23 +0200

    Add

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(
     cors({
     
-      origin: process.env.FRONTEND_URL,
+      origin: process.env.RENDER_URL,
       credentials: true,
       
 
